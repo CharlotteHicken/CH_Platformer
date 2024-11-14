e191cdf40e14a444b01cf219e6fa4a7d9def5c86
CharlotteHicken <hickecha@sheridancollege.ca>
2024-11-14 14:00:53 -0500

    created movement system

--- CH_Platformer/Assets/Scripts/PlayerController.cs
+++ CH_Platformer/Assets/Scripts/PlayerController.cs
@@ -8,11 +8,15 @@ public class PlayerController : MonoBehaviour
     {
         left, right
     }
+    Rigidbody2D rb;
+
+    public float moveSpeed;
+   
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,13 +24,13 @@ public class PlayerController : MonoBehaviour
     {
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+        Vector2 playerInput = new Vector2(Input.GetAxis("Horizontal"), 0);
         MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
-
+        rb.velocity = new Vector2(playerInput.x * moveSpeed, rb.velocity.y);
     }
 
     public bool IsWalking()
